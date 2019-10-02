b676d000b7428dac85a49aa0b6231ef0a1459e9f
Roger Lee <rmlee0003@gmail.com>
2019-10-01 21:44:16 -0400

    created initial state,completed task filter function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+const todoList = [
+  {
+    item: "Example",
+    id: Date.now(),
+    completed: false
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+      todos: todoList,
+      todo: ''
+    }
+  }
+  completed = event => {
+    event.preventDefault()
+    this.setState({
+      todos: this.state.todos.filter(task => {
+        return !task.completed
+      })
+    })
+  }
   render() {
     return (
       <div>
